5b0c7f265572ded2ade6afaa0cb7dd982ac31ec3
Mahadevanand <hhdheyeuwbw@gmail.com>
2026-08-15 19:03:54 +0530

    Implement Higher Lower Game with scoring system

---
+++ Higher__Lower_Game.py
@@ -0,0 +1,49 @@
+import random
+import os
+import game_art
+import game_database
+
+print(game_art.game_logo)
+score=0
+
+def display_accountinfo(account):
+    name = account["name"]
+    description = account["description"]
+    country = account["country"]
+    return f"{name}, a {description}, from {country}"
+
+def check_answer(guess, followers_count_1, followers_count_2):
+    if followers_count_1 > followers_count_2:
+        return guess == 1
+    else:
+        return guess == 2
+
+
+account_2 = random.choice(game_database.data)
+continue_flag = True
+while continue_flag:
+    account_1 = account_2
+    account_2 = random.choice(game_database.data)
+    while account_1 == account_2:
+        account_2 = random.choice(game_database.data)
+
+    print(f"Compare 1 : {display_accountinfo(account_1)}")
+    print(game_art.vs)
+    print(f"Compare 2 : {display_accountinfo(account_2)}")
+
+
+    guess = int(input("Who has more followers? 1 or 2 : "))
+
+    followers_count_1 = account_1["follower_count"]
+    followers_count_2 = account_2["follower_count"]
+
+    is_correct=check_answer(guess, followers_count_1, followers_count_2)
+    os.system("cls" if os.name == "nt" else "clear")
+    print(game_art.game_logo)
+    if is_correct:
+        score+=1
+        print(f"You are right. Your score is : {score}")
+    else:
+        print(f"You are wrong. Your final score is : {score}")
+        continue_flag = False
+give ,e this code readme
